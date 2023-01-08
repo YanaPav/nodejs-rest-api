b68e6bee96b36547c297b07316802e476859d127
Yana Pavlyk <pavlikyg@gmail.com>
2023-01-08 23:53:19 +0200

    image resize fn bug fixed

--- middlewares/prepareAvatar.js
+++ middlewares/prepareAvatar.js
@@ -1,20 +1,18 @@
 const { HttpError } = require("../helpers");
 const Jimp = require("jimp");
 
-const prepareAvatar = (req, res, next) => {
+const prepareAvatar = async (req, res, next) => {
   if (!req.file) {
     next(HttpError(400, "No image"));
   }
 
-  const { path: tempDir } = req.file;
-
-  Jimp.read(tempDir)
-    .then((image) => {
-      return image.resize(250, 250).write(tempDir);
-    })
-    .catch((err) => {
-      next(err);
-    });
+  try {
+    const { path: tempDir } = req.file;
+    const image = await Jimp.read(tempDir);
+    await image.resize(250, 250).write(tempDir);
+  } catch (error) {
+    next(error);
+  }
 
   next();
 };
